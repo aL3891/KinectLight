c1e6de1f688b318645ea58b00ca37192f33d2344
aL_3891 <aL_3891@hotmail.com>
2012-08-10 22:23:45 +0200

    Adding some stuff

---
+++ KinectLight.Core/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectLight.Core
+{
+    public class Class1
+    {
+    }
+
+    public struct apa {
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void banan() { 
+        
+        }
+
+    }
+}
