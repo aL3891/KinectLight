b6a20515ce60a806285a1142964c4fc3283674cd
aL_3891 <aL_3891@hotmail.com>
2012-08-12 19:19:24 +0200

    added soem game logic classes

---
+++ KinectLight.Core/MainGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Direct3D11;
+
+namespace KinectLight.Core
+{
+    public class MainGame : IDisposable
+    {
+        Stopwatch gameTime = new Stopwatch();
+        DeviceContext device = null;
+
+
+        public void Run() {
+            
+            while (true)
+            {
+                var currentTime = gameTime.ElapsedMilliseconds;
+        
+                Update(currentTime);
+                Render();
+            }
+
+        }
+
+        public void Update(long gameTime) { 
+        
+        }
+
+        public void Render() { 
+        
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KinectLight.Core/ThingBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+
+namespace KinectLight.Core
+{
+    public class ThingBase: IDisposable
+    {
+        Vector3 position, velocity;
+
+        public virtual void Render() { 
+        
+        }
+
+
+        public virtual void Initialize() { 
+        
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
